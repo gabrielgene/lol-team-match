858f070917579788748db98fda7c2b7de5f8e06b
Linzera <italo.lino@ucsal.edu.br>
2019-06-23 12:37:58 -0300

    Adding new filds to user model

--- db/models/user.js
+++ db/models/user.js
@@ -1,10 +1,33 @@
 const mongoose = require('mongoose');
 
+const CHAMPION_LIMIT = 5;
+
 const UserSchema = mongoose.Schema({
   name: String,
-  user: String,
-  pass: String,
-  phone: String,
+  email: String,
+  googleId: String,
+  password: String,
+  state: String,
+  leagueInfo: LeagueInfo
+});
+
+const Champions = mongoose.Schema({
+  name: String,
+  avatarUrl: String
+});
+
+const LeagueInfo = mongoose.Schema({
+  profileImage: String,
+  username: String,
+  mainChampions: {
+    type: [Champions],
+    validate: [CHAMPION_LIMIT, '{PATH} exceeds the limit of 5']
+  },
+  winRate: Number,
+  mainRole: { type: String, enum: ['MID', 'ADC', 'SUP', 'JG', 'TOP'] },
+  subRole: { type: String, enum: ['MID', 'ADC', 'SUP', 'JG', 'TOP'] },
+  tier: String,
+  rank: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
